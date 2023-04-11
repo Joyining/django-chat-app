572ea541621e5fda7627b97a05a0efc35cf65384
Joy Huang <joyhuang@kklab.com>
2023-04-11 18:28:45 +0800

    Modify timestamp default value and rename room to room_id

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+import django.utils.timezone
 
 # Create your models here.
 
@@ -13,9 +13,9 @@ class Room(models.Model):
 
 class Message(models.Model):
     value = models.CharField(max_length=10000, default='')
-    timestamp = models.DateTimeField(default=datetime.now())
+    timestamp = models.DateTimeField(default=django.utils.timezone.now)
     user_name = models.CharField(max_length=100, default='')
-    room = models.CharField(max_length=100, default='')
+    room_id = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return str(self.id)
