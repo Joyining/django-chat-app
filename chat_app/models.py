f637270d70e4da123df333f87e6c3ad53ca2422d
Joy Huang <joyhuang@kklab.com>
2023-04-11 16:48:31 +0800

    Add Room and Message models

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100, default='Untitled Room')
+
+
+class Message(models.Model):
+    value = models.CharField(max_length=10000, default='')
+    timestamp = models.DateTimeField(default=datetime.now())
+    user_name = models.CharField(max_length=100, default='')
+    room = models.CharField(max_length=100, default='')
